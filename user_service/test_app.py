3c5c515e3815cb6796b2590c4e48d835a9fb9079
HotCoffeeBean <vitalik-sush@mail.ru>
2026-04-27 06:18:10 +0300

    test_app.py and build.yml fix

--- user_service/test_app.py
+++ user_service/test_app.py
@@ -1,6 +1,16 @@
-from app import register
+from user_service.app import app, users
 
 def test_register():
-    data = {"username": "test", "password": "test"}
-    response = register(data)
+    users.clear()
+    client = app.test_client()
+
+    payload = {
+        "username": "Ivan",
+        "password": "0123456789"
+    }
+
+    response = client.post('/register', json=payload)
+
     assert response.status_code == 201
+    assert response.get_json()["msg"] == "User created"
+    assert users[-1]["username"] == "Ivan"
